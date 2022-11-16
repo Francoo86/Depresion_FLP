893182257c536e93db850a005ccba66cae0e1531
Ivan <115721146+Killl24@users.noreply.github.com>
2022-11-16 00:28:29 -0300

    Add files via upload

--- Script BD.sql
+++ Script BD.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     15/11/2022 23:34:06                          */
+/* Created on:     16/11/2022 0:23:05                           */
 /*==============================================================*/
 
 
@@ -10,17 +10,17 @@ drop table if exists CONTAINS_1;
 
 drop table if exists CREATES;
 
-drop table if exists PAIENT;
+drop table if exists PATIENTS;
 
-drop table if exists POLL;
+drop table if exists POLLS;
 
-drop table if exists PSYCHOLOGIST;
+drop table if exists PSYCHOLOGISTS;
 
 drop table if exists QUESTIONS;
 
 drop table if exists REPLYS;
 
-drop table if exists TEST;
+drop table if exists TESTS;
 
 drop table if exists USERS;
 
@@ -35,6 +35,7 @@ create table ANSWERS
    POINTS_A             int,
    TEXT_A               char(250),
    ATTRIBUTE_15         char(250),
+   DISABLE              bool,
    primary key (ID_ANSWERS)
 );
 
@@ -43,9 +44,9 @@ create table ANSWERS
 /*==============================================================*/
 create table CONTAINS_1
 (
-   ID_QUESTION          int not null,
    ID_TEST              int not null,
-   primary key (ID_QUESTION, ID_TEST)
+   ID_QUESTION          int not null,
+   primary key (ID_TEST, ID_QUESTION)
 );
 
 /*==============================================================*/
@@ -59,45 +60,53 @@ create table CREATES
 );
 
 /*==============================================================*/
-/* Table: PAIENT                                                */
+/* Table: PATIENTS                                              */
 /*==============================================================*/
-create table PAIENT
+create table PATIENTS
 (
    ID_USER              int not null,
-   ID_POLL              int not null,
+   ID_POLL              int,
    RUN                  int,
    NAMES                char(250),
    DESCRIPCION          char(250),
    PHONE_NUMBER         int,
    EMAIL                char(250),
+   DISABLE              bool,
+   PASSWORD_USERS       varchar(255),
+   LAST_NAME            char(250),
    SEX                  char(250),
    TOTAL_POINTS         int,
+   PASSPORT             char(250),
    primary key (ID_USER)
 );
 
 /*==============================================================*/
-/* Table: POLL                                                  */
+/* Table: POLLS                                                 */
 /*==============================================================*/
-create table POLL
+create table POLLS
 (
    ID_POLL              int not null,
+   DISABLE              bool,
    primary key (ID_POLL)
 );
 
 /*==============================================================*/
-/* Table: PSYCHOLOGIST                                          */
+/* Table: PSYCHOLOGISTS                                         */
 /*==============================================================*/
-create table PSYCHOLOGIST
+create table PSYCHOLOGISTS
 (
    ID_USER              int not null,
-   ID_POLL              int not null,
+   ID_POLL              int,
    RUN                  int,
    NAMES                char(250),
    DESCRIPCION          char(250),
    PHONE_NUMBER         int,
    EMAIL                char(250),
+   DISABLE              bool,
+   PASSWORD_USERS       varchar(255),
+   LAST_NAME            char(250),
    DIAGNOSTIC_PATIENT   char(250),
-   ESPECIALIDAD         char(250),
+   SPECIALTY            char(250),
    primary key (ID_USER)
 );
 
@@ -109,7 +118,7 @@ create table QUESTIONS
    ID_QUESTION          int not null,
    QUESTION             char(250),
    DESCRIPCION_QUESTION char(250),
-   DELETE_AT            char(250),
+   DISABLE              bool,
    primary key (ID_QUESTION)
 );
 
@@ -124,15 +133,16 @@ create table REPLYS
 );
 
 /*==============================================================*/
-/* Table: TEST                                                  */
+/* Table: TESTS                                                 */
 /*==============================================================*/
-create table TEST
+create table TESTS
 (
    ID_TEST              int not null,
    ID_POLL              int not null,
    RESULTS              char(250),
    TOTAL_POINTS         int,
    QUESTIONS            char(250),
+   DISABLE              bool,
    primary key (ID_TEST)
 );
 
@@ -148,42 +158,45 @@ create table USERS
    DESCRIPCION          char(250),
    PHONE_NUMBER         int,
    EMAIL                char(250),
+   DISABLE              bool,
+   PASSWORD_USERS       varchar(255),
+   LAST_NAME            char(250),
    primary key (ID_USER)
 );
 
-alter table ANSWERS add constraint FK_CONTAINS_3 foreign key (ID_POLL)
-      references POLL (ID_POLL) on delete restrict on update restrict;
+alter table ANSWERS add constraint FK_CONTAINS_4 foreign key (ID_POLL)
+      references POLLS (ID_POLL) on delete restrict on update restrict;
 
 alter table ANSWERS add constraint FK_HAVES foreign key (ID_QUESTION)
       references QUESTIONS (ID_QUESTION) on delete restrict on update restrict;
 
-alter table CONTAINS_1 add constraint FK_CONTAINS_1 foreign key (ID_QUESTION)
+alter table CONTAINS_1 add constraint FK_CONTAINS_1 foreign key (ID_TEST)
+      references TESTS (ID_TEST) on delete restrict on update restrict;
+
+alter table CONTAINS_1 add constraint FK_CONTAINS_2 foreign key (ID_QUESTION)
       references QUESTIONS (ID_QUESTION) on delete restrict on update restrict;
 
-alter table CONTAINS_1 add constraint FK_CONTAINS_5 foreign key (ID_TEST)
-      references TEST (ID_TEST) on delete restrict on update restrict;
+alter table CREATES add constraint FK_CREATES foreign key (ID_USER)
+      references PSYCHOLOGISTS (ID_USER) on delete restrict on update restrict;
 
-alter table CREATES add constraint FK_CREATES foreign key (ID_TEST)
-      references TEST (ID_TEST) on delete restrict on update restrict;
+alter table CREATES add constraint FK_CREATES2 foreign key (ID_TEST)
+      references TESTS (ID_TEST) on delete restrict on update restrict;
 
-alter table CREATES add constraint FK_CREATES2 foreign key (ID_USER)
-      references PSYCHOLOGIST (ID_USER) on delete restrict on update restrict;
-
-alter table PAIENT add constraint FK_CAN_BE2 foreign key (ID_USER)
+alter table PATIENTS add constraint FK_CAN_BE2 foreign key (ID_USER)
       references USERS (ID_USER) on delete restrict on update restrict;
 
-alter table PSYCHOLOGIST add constraint FK_CAN_BE foreign key (ID_USER)
+alter table PSYCHOLOGISTS add constraint FK_CAN_BE foreign key (ID_USER)
       references USERS (ID_USER) on delete restrict on update restrict;
 
-alter table REPLYS add constraint FK_REPLYS foreign key (ID_TEST)
-      references TEST (ID_TEST) on delete restrict on update restrict;
+alter table REPLYS add constraint FK_REPLYS foreign key (ID_USER)
+      references PATIENTS (ID_USER) on delete restrict on update restrict;
 
-alter table REPLYS add constraint FK_REPLYS2 foreign key (ID_USER)
-      references PAIENT (ID_USER) on delete restrict on update restrict;
+alter table REPLYS add constraint FK_REPLYS2 foreign key (ID_TEST)
+      references TESTS (ID_TEST) on delete restrict on update restrict;
 
-alter table TEST add constraint FK_CONTAINS_4 foreign key (ID_POLL)
-      references POLL (ID_POLL) on delete restrict on update restrict;
+alter table TESTS add constraint FK_CONTAINS_5 foreign key (ID_POLL)
+      references POLLS (ID_POLL) on delete restrict on update restrict;
 
-alter table USERS add constraint FK_CONTAINS_2 foreign key (ID_POLL)
-      references POLL (ID_POLL) on delete restrict on update restrict;
+alter table USERS add constraint FK_CONTAINS_3 foreign key (ID_POLL)
+      references POLLS (ID_POLL) on delete restrict on update restrict;
 
